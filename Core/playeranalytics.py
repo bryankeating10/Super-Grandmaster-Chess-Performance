c30c88838c7fad4dbd21de8fb3700f5a3ffea8e4
Bryan Keating <ru.bryankeating@gmail.com>
2025-11-02 16:17:57 -0500

    PlayerAnalytics skeleton

---
+++ Core/playeranalytics.py
@@ -0,0 +1,12 @@
+class PlayerAnalytics:
+    def __init__(self, metadata_df, moves_dict):
+        self.metadata_df = metadata_df
+        self.moves_dict = moves_dict
+
+    def player_stats(self, player_name):
+        # return summary statistics for a player
+        pass
+
+    def opening_performance(self, player_name):
+        # aggregate outcomes by ECO or opening name
+        pass
